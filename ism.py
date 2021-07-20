1882aac1887b11309154fbf76a7c62f1dac5f09e
Krajczár Dávid <redline@manjaro.t.hu>
2021-07-20 16:58:01 +0200

    Work

--- ism.py
+++ ism.py
@@ -1,17 +1,24 @@
 from os import system, path
 from sys import argv, argc
 
+def install(package_dir: str):
+    system(f"cp {package_dir} /usr/share/redline_ism/{package_dir}")
+    system(f"sudo sh {package_dir}/install.sh")
+
 if argc > 1:
     argument = argv[1]
+
     if argument == "install":
-        package_dir = argv[2]
-        if path.isfile(f"{package_dir}/install.sh"):
-            system(f"cp {package_dir} /usr/share/redline_ism/{package_dir}")
-            system(f"sudo sh {package_dir}/install.sh")
+        if path.isfile(f"{argv[2]}/install.sh"):
+            if(input(f"Install {argv[2]} systemwide? y/n").lower == "y"):
+                install(argv[2])
+        
     elif argument == "remove":
         if(input(f"Remove {package_name}? y/n").lower == "y"):
             package_name = argv[2]
             if path.isfile(f"/usr/share/redline_ism/{package_name}/remove.sh"):
                 system(f"sudo sh /usr/share/redline_ism/{package_name}/remove.sh")
+    elif argument == "full-remove":
+
 else:
     print("Usage:\nism install <package_folder>: install package from downloaded folder")
